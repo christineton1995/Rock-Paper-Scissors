9015110cb08769453fef174746453f2855ca6d71
Vinh Quyen <vinhquyenton.1995@gmail.com>
2022-11-29 17:51:31 -0800

    Update playerChoice with choices

--- main.js
+++ main.js
@@ -12,18 +12,30 @@ function playRound() {
 function playerChoice() {
     let input = prompt("Rock, Paper, or Scissors?");
     while (input == null) {
-        input = prompt("Rock, Paperm or Scissors?");
+        input = prompt("Rock, Paper or Scissors?");
     }
     input = input.toLowerCase();
-    
+    let check = validateInput(input);
+    while (check == false) {
+        prompt("Type Rock, Paper, or Scissors. Spelling matters, but capitalization doesn't!");
+    }
 
 
-    console.log(input);
+    //console.log(input);
 
 }
 
-function validateInput ();
+function validateInput(choice) {
+    if (choices.includes(choice)) {
+        return true
+    }
+    else {
+        return false
+    }
+};
 
 function computerChoice() {
     return choices[Math.floor(Math.random *choices.length)];
 }
+
+game();

8068e39e8bbac089db9593f0ba05f4e5ad51777c
Vinh Quyen <vinhquyenton.1995@gmail.com>
2022-11-29 17:18:37 -0800

    Create computerChoice function

---
+++ main.js
@@ -0,0 +1,19 @@
+const choices = ["rock","paper","scissors"]
+
+function game() {
+    //play the game
+    //play five rounds 
+    //console based
+}
+
+function playRound() {
+    const playerSelection =
+}
+
+function playerChoice() {
+    //get input from player
+}
+
+function computerChoice() {
+    return choices[Math.floor(Math.random *choices.length)];
+}

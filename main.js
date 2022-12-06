0e4982a78ba6f59846b743afa5bfb19a1a07598a
Vinh Quyen <vinhquyenton.1995@gmail.com>
2022-12-05 19:06:51 -0800

    Add functionality to Rock button

--- main.js
+++ main.js
@@ -1,69 +1,39 @@
 const choices = ["rock","paper","scissors"]
 
-/*function game() {
-    for (let i = 0; i<=5;i++) {
-        playRound();
-
-    }
-} */
 
 // variables for storing scores
 let playerScore = 0;
 let computerScore = 0;
 
+let playerSelection;
+let computerSelection = computerChoice ();
+
 
 //function to play one round, and keep track of score
-function playRound() {
-    let playerSelec = choices;
-    let computerSelec = computerChoice();
-    if (playerSelec == computerSelec) {
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection == computerSelection) {
         return "Tie";
-    } else if (playerSelec == "rock" && computerSelec == "scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++;
         return "Player wins. Rock beats scissors.";
-    } else if (playerSelec == "paper" && computerSelec == "rock") {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++;
         return "Player wins. Paper beats rock.";
-    } else if (playerSelec == "scissors" && computerSelec == "paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore++;
         return "Player wins. Scissors beat paper.";
-    } else if (playerSelec == "rock" && computerSelec == "paper") {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore++;
         return "Computer wins. Paper beats rock.";
-    } else if (playerSelec == "paper" && computerSelec == "scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore++;
         return "Computer wins. Scissors beat paper.";
-    } else if (playerSelec == "scissors" && computerSelec == "rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore++;
         return "Computer wins. Rock beats scissors.";
     }
 
 }
-
-/*function playerChoice() {
-    let input = prompt("Rock, Paper, or Scissors?");
-    while (input == null) {
-        input = prompt("Rock, Paper, or Scissors?");
-    }
-    input = input.toLowerCase();
-    let check = validateInput(input);
-    while (check == false) {
-        input = prompt("Type Rock, Paper, or Scissors. Spelling matters, but capitalization doesn't!");
-        input = input.toLowerCase();
-        check = validateInput(input);
-      }
-    return input; 
-
-} 
-
-function validateInput(choice) {
-    if (choices.includes(choice)) {
-        return true
-    }
-    else {
-        return false
-    }
-}; */
 
 function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -71,10 +41,6 @@ function computerChoice() {
 
 // Adding functionality to Rock Button 
 const rButton = document.getElementById("rBtn");
-rButton.addEventListener('click', () => {
-    let playerSelec = "rock";
-    playRound();
+rButton.addEventListener("click", () => {
+    console.log (playRound("rock",computerChoice()));
 });
-
-
-//game();
